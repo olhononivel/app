75c8b7f4ec9d73bde22e2810cc15afddf3306df9
olhononivel <olhononivel@gmail.com>
2025-07-22 00:33:19 -0300

    feat: link logi

--- src/components/logo/index.tsx
+++ src/components/logo/index.tsx
@@ -10,7 +10,7 @@ interface LogoProps {
 
 const Logo = ({ isFooter = false }: LogoProps) => {
 	return (
-		<Link href="/home" className="flex items-center gap-2">
+		<Link href="/" className="flex items-center gap-2">
 			<Image src={isFooter ? LogoImageFooter : LogoImage} alt="Olho no nível" width={60} height={6} className="h-16" />
 			<h1 className={`text-transparent bg-clip-text bg-gradient-to-r ${isFooter ? "from-blue-500" : "from-gray-900"} via-blue-800 to-purple-900 font-bold text-xl`}>
 				Olho no nível
